6cecf64d1b719a4a857921d2c4af158edf09e630
OrTab <or.1532.tabin@gmail.com>
2025-01-07 04:54:48 +0200

    create useWindowSize hook

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { breakpoints } from "../style/breakpoints";
+
+export interface IWindowSize {
+  width: number;
+  height: number;
+}
+
+const getSize = () => {
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+};
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<IWindowSize>(() => getSize());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return {
+    windowSize,
+    setWindowSize,
+    isUpToSmall: windowSize.width <= breakpoints.small,
+    isSmallUp: windowSize.width > breakpoints.small,
+    isMediumUp: windowSize.width > breakpoints.medium,
+    isLargeUp: windowSize.width > breakpoints.large,
+    isXLargeUp: windowSize.width > breakpoints.xLarge,
+    isLowHeight: windowSize.height < 800,
+  };
+}
